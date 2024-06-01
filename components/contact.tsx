f27b5ca04e92ed22e43a87dd1ee25384babac73a
karanyadavv <karanyadav729@gmail.com>
2024-06-01 19:11:48 +0530

    dark mode

--- components/contact.tsx
+++ components/contact.tsx
@@ -30,12 +30,12 @@ export default function Contact(){
       }}
     >
       <SectionHeading>Contact me</SectionHeading>
-      <p className="text-gray-700 -mt-6">
+      <p className="text-gray-700 -mt-6 dark:text-white/80">
         Please contact me directly at <a href="mailto:karanyadav729@gmail.com" className="underline">karanyadav729@gmail.com</a> or through this form.
       </p>
 
       <form 
-        className="mt-10 flex flex-col" 
+        className="mt-10 flex flex-col dark:text-black" 
         action={async formData => {
           const { data, error } = await sendEmail(formData);
 
@@ -53,14 +53,14 @@ export default function Contact(){
           name="senderEmail"
           required
           maxLength={500}
-          className="px-4 h-14 rounded-lg borderBlack"
+          className="px-4 h-14 rounded-lg borderBlack dark:bg-white dark:bg-opacity-80 dark:focus:bg-opacity-100 transition-all dark:outline-none"
         />
         <textarea 
           placeholder="Your message"
           name="message"
           required
           maxLength={5000}
-          className="h-52 my-3 rounded-lg borderBlack p-4" 
+          className="h-52 my-3 rounded-lg borderBlack p-4 dark:bg-white dark:bg-opacity-80 dark:focus:bg-opacity-100 transition-all dark:outline-none" 
         />
         <SubmitBtn />
       </form>
--- components/intro.tsx
+++ components/intro.tsx
@@ -66,15 +66,15 @@ export default function Intro(){
         </Link>
 
         <a
-          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition-all borderBlack"
-          href="/cv.pdf"
+          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition-all borderBlack dark:bg-white/10"
+          href="/karanyadav-cv.pdf"
           download
         >
           Download CV <HiDownload className="opacity-60 group-hover:translate-y-1 transition"/> 
         </a>
 
         <a
-          className="bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full text-[1.35rem] focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack"
+          className="bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full text-[1.35rem] focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
           href="https://linkedin.com/in/karan-yadav-1982b0150/"
           target="_blank"
         >
@@ -82,7 +82,7 @@ export default function Intro(){
         </a>
 
         <a
-          className="bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full text-[1.35rem] focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack"
+          className="bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full text-[1.35rem] focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
           href="https://github.com/karanyadavv"
           target="_blank"
         >
