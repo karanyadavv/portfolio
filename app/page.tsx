7d04d33feafe6bebbce92655d00fcb94dc3223c6
karanyadavv <karanyadav729@gmail.com>
2025-09-12 20:29:58 +0530

    deleting about section

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-import About from "@/components/about";
-import Contact from "@/components/contact";
 import Experience from "@/components/experience";
 import Intro from "@/components/intro";
 import Projects from "@/components/projects";
@@ -11,11 +9,9 @@ export default function Home() {
     <main className="flex flex-col items-center px-4">
       <Intro />
       <SectionDivider />
-      {/* <About /> */}
       <Projects />
       <Skills />
       <Experience />
-      {/* <Contact /> */}
     </main>
   );
 }
