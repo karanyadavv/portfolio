5e30127b32f1fd7875975dcd7f073667003b3f60
karanyadavv <karanyadav729@gmail.com>
2025-12-22 23:28:26 +0530

    fixed nav resp width & added discord-user-area comp initial template

--- app/components/discord-user-area/page.tsx
+++ app/components/discord-user-area/page.tsx
@@ -1,7 +1,13 @@
 "use client";
+import Image from "next/image";
 import { useRouter } from "next/navigation";
 import React from "react";
 import { IoArrowBack } from "react-icons/io5";
+import karan from "@/public/karan.jpg";
+import { IoIosMicOff } from "react-icons/io";
+import { FaChevronDown } from "react-icons/fa";
+import { TbHeadphonesOff } from "react-icons/tb";
+import { RiSettings5Fill } from "react-icons/ri";
 
 const DiscordUserArea = () => {
   const router = useRouter();
@@ -13,11 +19,69 @@ const DiscordUserArea = () => {
     <div className="px-6">
       <div
         onClick={handleBack}
-        className="mb-8 flex cursor-pointer items-center gap-2 hover:underline hover:underline-offset-8"
+        className="mb-8 flex cursor-pointer items-center gap-2 text-zinc-500 hover:underline hover:underline-offset-8"
       >
-        <IoArrowBack /> Components
+        <IoArrowBack size={16} color="currentColor" /> Components
       </div>
-      <div>DiscordUserArea</div>
+      <div className="flex flex-col gap-6">
+        <h1 className="text-3xl font-semibold tracking-tighter">
+          Discord User Area
+        </h1>
+        <div className="relative mb-20 h-96 w-full rounded-md border border-border">
+          <div className="text-shadow-md absolute inset-0 flex items-center justify-center rounded-md bg-white/30 text-9xl font-bold text-white/90">
+            WIP
+          </div>
+          <div className="flex h-full items-center justify-center">
+            <div className="flex h-[60px] w-[360px] items-center gap-0.5 rounded-md bg-[#202024] px-2 py-4">
+              <div className="flex cursor-pointer items-center gap-2 py-[2px] pl-[4px] pr-32 hover:rounded-md hover:bg-[#333338]">
+                <div>
+                  <Image
+                    src={karan}
+                    height={32}
+                    width={32}
+                    alt="Profile photo"
+                    className="rounded-full"
+                  />
+                </div>
+                <div className="flex flex-col gap-[0.5px]">
+                  <div className="text-[0.930rem] font-semibold text-zinc-300">
+                    Karan
+                  </div>
+                  <div className="text-[0.70rem] font-semibold text-[#7f7f86]">
+                    Online
+                  </div>
+                </div>
+              </div>
+              {/* Mic */}
+              <div className="flex h-[28px] gap-0.5">
+                <IoIosMicOff
+                  size={28}
+                  className="cursor-pointer rounded-s-md bg-[#352027] px-[2px] py-[2px] text-[#a92f3d]"
+                />
+                <FaChevronDown
+                  size={2}
+                  className="h-100vh h-full w-[25%] cursor-pointer rounded-r-md bg-[#352027] text-[#a92f3d]"
+                />
+              </div>
+              {/* Deafen */}
+              <div className="flex h-[28px] gap-0.5">
+                <TbHeadphonesOff
+                  size={28}
+                  className="cursor-pointer rounded-s-md bg-[#352027] px-[2px] py-[2px] text-[#a92f3d]"
+                />
+                <FaChevronDown
+                  size={2}
+                  className="h-100vh h-full w-[25%] cursor-pointer rounded-r-md bg-[#352027] text-[#a92f3d]"
+                />
+              </div>
+              <RiSettings5Fill
+                size={24}
+                className="cursor-pointer text-[#aaaab1] hover:bg-[#333338] hover:text-[#f4f4f4]"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- components/projects.tsx
+++ components/projects.tsx
@@ -5,24 +5,19 @@ import { projectsData } from "@/lib/data";
 import Project from "./project";
 import { useSectionInView } from "@/lib/hooks";
 
-
 export default function Projects() {
-  const { ref } =  useSectionInView("Projects", 0.5);
-  
+  const { ref } = useSectionInView("Projects", 0.5);
 
-  return(
-    <section ref={ref} id="projects" className="scroll-mt-28 mb-28">
+  return (
+    <section ref={ref} id="projects" className="mb-28 scroll-mt-28 px-2">
       <SectionHeading>My projects</SectionHeading>
       <div>
-        {
-          projectsData.map((project, index) => (
-            <React.Fragment key={index}>
-              <Project {...project}/>
-            </React.Fragment>
-          ))
-        }
+        {projectsData.map((project, index) => (
+          <React.Fragment key={index}>
+            <Project {...project} />
+          </React.Fragment>
+        ))}
       </div>
     </section>
-  )
+  );
 }
-
