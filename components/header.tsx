60f3d67b063378185cd3cc8a95c67ed5f80dffc4
karanyadavv <karanyadav729@gmail.com>
2026-05-23 21:39:02 +0530

    updated nav

--- components/header.tsx
+++ components/header.tsx
@@ -10,26 +10,19 @@ export default function Header() {
     useActiveSectionContext();
 
   return (
-    <header className="relative z-[999]">
-      <motion.div
-        className="fixed left-1/2 top-0 h-[4.5rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] dark:border-black/40 dark:bg-gray-950 dark:bg-opacity-75 sm:top-6 sm:h-[3.25rem] sm:w-[24rem] sm:rounded-md md:w-2/3 lg:w-1/2 xl:w-2/5"
-        initial={{ y: -100, x: "-50%", opacity: 0 }}
-        animate={{ y: 0, x: "-50%", opacity: 1 }}
-      ></motion.div>
-      <nav className="fixed left-1/2 top-[0.15rem] flex h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0">
-        <ul className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-500 sm:w-[initial] sm:flex-nowrap sm:gap-5">
+    <header className="z-10">
+      <nav className="pointer-events-auto fixed left-1/2 top-8 z-[999] flex -translate-x-1/2 items-center justify-between rounded-lg border border-neutral-200 bg-white/70 p-1 shadow-md backdrop-blur-md dark:border-neutral-800 dark:bg-black/70 sm:top-4">
+        <ul className="flex items-center justify-center gap-y-1 text-sm font-medium text-gray-500 sm:flex-nowrap sm:gap-5">
           {links.map((link) => (
             <motion.li
-              className="relative flex h-3/4 items-center justify-center"
+              className="relative flex items-center justify-center"
               key={link.hash}
-              initial={{ y: -100, opacity: 0 }}
-              animate={{ y: 0, opacity: 1 }}
             >
               <Link
                 className={clsx(
                   "flex w-full items-center justify-center px-3 py-3 transition hover:text-gray-950 dark:text-gray-500 dark:hover:text-gray-300",
                   {
-                    "text-gray-950 dark:text-gray-200":
+                    "text-gray-800 dark:text-gray-100":
                       activeSection === link.name,
                   },
                 )}
@@ -42,12 +35,12 @@ export default function Header() {
                 {link.name}
                 {link.name === activeSection && (
                   <motion.span
-                    className="absolute inset-0 -z-10 rounded-md bg-gray-100 dark:bg-gray-800"
+                    className="absolute inset-x-1 inset-y-1 -z-10 rounded-[4px] bg-neutral-200 dark:bg-neutral-800"
                     layoutId="activeSection"
                     transition={{
                       type: "spring",
                       stiffness: 380,
-                      damping: 30,
+                      damping: 35,
                     }}
                   ></motion.span>
                 )}
